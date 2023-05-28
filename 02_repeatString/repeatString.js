b1535295f6ee6eaa12000f975fe0dc2a35b2a5f5
Bradley <bradleydoyle24@gmail.com>
2023-05-27 21:19:08 -0400

    Completed assignment by modifying repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  if (num < 0) return "ERROR";
+  let newString = "";
+  for (let i = 0; i < num; i++) {
+    newString += string;
+  }
+  return newString;
 };
+
+//create holder for string
+//add string to holder (i)x
+//return holder
 
 // Do not edit below this line
 module.exports = repeatString;
