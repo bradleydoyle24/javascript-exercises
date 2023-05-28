4ce2173517570a3a402287f3d246a52794d81f94
Bradley <bradleydoyle24@gmail.com>
2023-05-28 16:40:27 -0400

    Complete assignment by modifying tempConversion.js file

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+  // (F - 32) / 1.8
+  // round to 1 decimal place
+  let celsius = (Math.round(((fTemp - 32) / 1.8) * 10) / 10);
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+  let fahrenheit = (Math.round(((cTemp * 1.8) + 32) * 10) / 10);
+  return fahrenheit;
 };
 
 // Do not edit below this line
