8c669566cd59faa313f3f781278c80df3a57ecf7
Bradley <bradleydoyle24@gmail.com>
2023-05-27 23:06:17 -0400

    Complete 04-removeFromArray with my own solution
    My own solution was different than the supplied solution.  The
    supplied solution would presumably run quicker, as my solution
    used 2 nested 'for' loops with a runtime of O(n^2).  The supplied
    solution only iterates through the array one time, so O(n).

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+  let length = array.length;
 
+  for (i = 0; i < args.length; i++){
+    for (j = 0; j < length; j++) {
+      if (array[j] === args[i]) {
+        array.splice(j, 1);
+      }
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
