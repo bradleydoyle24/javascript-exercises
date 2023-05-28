3dc72f4dc00c34508a91607b2f3275b9120f4fdd
Bradley <bradleydoyle24@gmail.com>
2023-05-27 22:14:39 -0400

    Create my solution for reverseString.js, which is apparantly much longer than the intended solution :(

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,18 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  const reversedArray = [];
 
+  let length = string.length;
+  let reversedString = "";
+
+  for (i = length - 1; i >= 0; i--) {
+    reversedArray.push(string.charAt(i));
+  }
+  
+  for (i = 0; i < length; i++) {
+    reversedString += reversedArray[i];
+  }
+
+  return reversedString;
 };
 
 // Do not edit below this line
