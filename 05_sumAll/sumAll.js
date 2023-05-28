7dca25eeb0ac723bf7f4440e9b9fed45c8ac81a5
Bradley <bradleydoyle24@gmail.com>
2023-05-27 23:36:40 -0400

    Complete 05-sumAll assignment
    Personal solution only checked that the arguments === typeof 'number'.
    Supplied solution used '!Number.isInteger(argument)' which would also
    account for decimals, which my program would have missed.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  let sum = 0;
 
+  // Ensure numbers are entered
+  if (typeof num1 !== "number" || typeof num2 !== "number") return "ERROR";
+  
+
+  // Ensure no negative numbers
+  if (num1 < 0 || num2 < 0) return "ERROR";
+
+  // Reorder numbers if larger number first.
+  if (num1 > num2) {
+    let tmp = num2;
+    num2 = num1;
+    num1 = tmp;
+  }
+
+  for (i = num1; i <= num2; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
