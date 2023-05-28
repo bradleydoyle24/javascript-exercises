1ff44266162862f51806c5a688203ce85a7c72d0
Bradley <bradleydoyle24@gmail.com>
2023-05-28 16:02:26 -0400

    Complete exercise for 06-leap-years
    Completed exercise with my own solution.  Wrote a triple-nested
    'if' loop to see if it could work.  It did, but doing one line
    with && / || would be more efficient.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
+const leapYears = function(year) {
+  // divisible by 4 = true 
+  // divisible by 100 = not
+  // Divisible by 400 = true
 
+  // Start with / 100, as those are not leap years
+    // But if divisible by 100 && 400, it IS a leap year
+
+  if (year % 4 === 0) {
+    if (year % 100 === 0) {
+      if (year % 400 === 0) {
+        return true;
+      } else {return false};
+    } else {return true;}
+  } else {return false;}
 };
 
 // Do not edit below this line
