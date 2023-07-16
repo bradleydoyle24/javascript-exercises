eb05ea110a2eb6ce19fac23b8192baf845ad38f9
Bradley <bradleydoyle24@gmail.com>
2023-07-16 17:16:00 -0400

    Completed assignment with my solution

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((accumulator, item) => accumulator + item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((accumulator, item) => accumulator * item, 1)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let result = 1;
+	while (number > 0) {
+    result = number * result;
+    number--;
+  }
+  return result;
 };
 
 // Do not edit below this line
