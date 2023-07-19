94206c4cb5cbf0a20aabaa1e43002fea305add23
Bradley <bradleydoyle24@gmail.com>
2023-07-18 22:14:32 -0400

    Create personal soultion.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,33 @@
-const palindromes = function () {
 
+const palindromes = function(string) {
+  let reducedArray = [];
+  let reversedArray = [];
+  lowerString = string.toLowerCase();
+
+  for(i = 0; i < lowerString.length; i++) {
+    let temp = lowerString.codePointAt(i);
+    // Checks if letter or number. Places only letters or numbers in new arrays.
+    // Function is supposed to ignore spaces and punctuation.
+    if ((temp >= 48 && temp <= 57) ||
+        (temp >= 65 && temp <= 90) ||
+        (temp >= 97 && temp <= 122)) {
+      reducedArray.push(temp);
+      reversedArray.unshift(temp);
+    }
+  }
+  let wrongCount = 0;
+  for(i = 0; i < reducedArray.length; i++) {
+    if (reducedArray[i] !== reversedArray[i]) {
+      wrongCount++;
+    }
+  }
+  if (wrongCount === 0) {
+    return true;
+  }
+  else return false;
 };
+
+console.log(palindromes);
 
 // Do not edit below this line
 module.exports = palindromes;
