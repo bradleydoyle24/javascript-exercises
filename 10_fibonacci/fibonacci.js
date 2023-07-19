8bcc44d942d3f0f89c16d423ee019d78dbe7ab83
Bradley <bradleydoyle24@gmail.com>
2023-07-18 22:35:23 -0400

    Finish exercise with preliminary code
    Current function seems messy.  Hard-coded value to return if
    argument is '1'.  Could be worked into algorithm.  However,
    it currently passes all tests.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+  let a = 0;
+  let b = 1;
+  let c;
+  if (number == 1) return 1;
+  if (number >= 0){  
+    for (i = 1; i < number; i++) {
+      c = a + b;
+      a = b;
+      b = c;
+    }
+  return c;
+  }
+  else return "OOPS";
 };
 
 // Do not edit below this line
