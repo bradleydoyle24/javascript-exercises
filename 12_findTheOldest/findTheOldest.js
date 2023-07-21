1dc7ad49fca1cd3211990fe48145d44aa933e07b
Bradley <bradleydoyle24@gmail.com>
2023-07-21 16:08:19 -0400

    Completed assignment with my own solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,30 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+  /*
+  Search through list
+  Make variable of oldest age
+  Find age of person
+    age of death - age of birth
+  Compare ages to oldest age variable
+  If over, change oldest age variable
+  Return people[i] of oldest person
+  */
+
+ const date = new Date();
+ const currentYear = date.getFullYear();
+
+  let oldestAge = 0;
+  let oldestPerson;
+  for(i = 0; i < array.length; i++) {
+   if (array[i].yearOfDeath === undefined) {
+      array[i].yearOfDeath = currentYear;
+    }
+    let age = array[i].yearOfDeath - array[i].yearOfBirth;
+    if (age > oldestAge) {
+      oldestAge = age;
+      oldestPerson = array[i];
+    }
+  }
+  return oldestPerson;
 
 };
 
